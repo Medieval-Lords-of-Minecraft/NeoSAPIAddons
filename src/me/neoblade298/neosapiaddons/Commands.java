25886eb95a0aa72219c167a547a39fb16e623ad6
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-01-27 11:03:41 -0800

    Update neocore commands

--- src/me/neoblade298/neosapiaddons/Commands.java
+++ src/me/neoblade298/neosapiaddons/Commands.java
@@ -5,7 +5,7 @@ import com.sucy.skill.api.player.PlayerSkill;
 import com.sucy.skill.api.skills.Skill;
 import com.sucy.skill.api.util.FlagManager;
 
-import me.neoblade298.neocore.bukkit.util.BukkitUtil;
+import me.neoblade298.neocore.bukkit.util.Util;
 import me.neoblade298.neocore.shared.util.SharedUtil;
 import me.neoblade298.neosapiaddons.methods.ProfessClass;
 import me.neoblade298.neosapiaddons.methods.ResetClass;
@@ -75,9 +75,9 @@ public class Commands implements CommandExecutor {
 				String skill = SharedUtil.connectArgs(args, 2);
 				PlayerSkill ps = SkillAPI.getPlayerData(p).getSkill(skill);
 				if (ps == null) {
-					BukkitUtil.msg(sender, "&cThis skill doesn't exist for this player!");
+					Util.msg(sender, "&cThis skill doesn't exist for this player!");
 				}
-				BukkitUtil.msg(sender, "&7Player &c" + p.getName() + " &7skill level of &6" + ps.getData().getName() + " &7is &e" + ps.getLevel());
+				Util.msg(sender, "&7Player &c" + p.getName() + " &7skill level of &6" + ps.getData().getName() + " &7is &e" + ps.getLevel());
 				return true;
 			}
 			else if (args[0].equalsIgnoreCase("flag")) {
